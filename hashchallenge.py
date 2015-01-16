072ce46adb3512b07e7126be4d40011d969b007d
dtwitch <twitched@gmail.com>
2015-01-16 15:24:32 -0600

    Update to use URLs for wordlists rather than files

--- hashchallenge.py
+++ hashchallenge.py
@@ -24,16 +24,16 @@ def file_description():
     return """
     The challenge file will have one challenge per line in the following format
         
-        type [hashcat_mask|num_words] algorithm description [file] [file_size] [points]
+        type points (hashcat_mask|num_words) algorithm description [url] [max_word]
     
     type is either h or w.  h is a hashcat mask, w is words from a file
+    points - the number of points the challenge is worth
     hashcat_mask - a mask in hashcat format if the type is h.
     num_words - the number of words from the given file if the type is w
     algorithm - one of md5, sha1, sha256, bcrypt, or scrypt
     description - a description of the challenge
-    file - the file from which the words will be obtained with one word per line  This argument is required if the type is 'w'
-    file_size - the size of the file given.  This argument is required if a file is given
-    points - the number of points the challenge is worth
+    url - URL of the file from which the words will be obtained with one word per line  This argument is required if the type is 'w'
+    max_word - the size of the file given or the highest line to be read in the file.  This argument is required if a file is given
     """
 
 if __name__ == '__main__':
