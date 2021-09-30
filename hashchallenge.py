2ae1df7691e8e09b0cff11f75b2895127ed93204
Doug Twitchell <twitched@gmail.com>
2021-09-29 18:00:32 -0600

    Output to csv

--- hashchallenge.py
+++ hashchallenge.py
@@ -4,7 +4,7 @@
 Create hash cracking challenges
 """
 
-import os, sys, argparse, shlex, challenge, collections, tabulate
+import os, sys, argparse, shlex, challenge, collections, csv
 
 pwd = os.getcwd()
 
@@ -20,8 +20,6 @@ def parse_args(argv):
     group.add_argument('--number', default=2, type=int, help="number of challenges to create")
     parser.add_argument('--challenge-file', type=argparse.FileType('r', encoding='utf-8'), required=True, help="File containing a list of challenges to add")
     parser.add_argument('--output-dir', required=True, help="Directory where output files will go")
-    parser.add_argument('--header-file', type=argparse.FileType('r', encoding='utf-8'), help="File containing a header to go before the table of challenges")
-
     return parser.parse_args(argv)
 
 def file_description():
@@ -42,8 +40,6 @@ def file_description():
     """
 
 def create_challenge_files(args):
-    header = args.header_file.read()
-    args.header_file.close()
     if args.namelist:
         for name in args.namelist:
             challenges = generate_challenges(args.challenge_file)
@@ -57,31 +53,25 @@ def create_challenge_files(args):
             solutiondir = os.path.join(pwd, args.output_dir, "solutions")
             if not os.path.exists(solutiondir):
                 os.makedirs(solutiondir)
-            write_challenges_to_file(challenges, open(os.path.join(challengenamedir, name.strip() + ".md"), 'w+'), header, False)
-            write_challenges_to_file(challenges, open(os.path.join(solutiondir, name.strip() + "_solution.md"), 'w+'), header, True)
+            write_challenges_to_file(challenges, os.path.join(challengenamedir, name.strip() + ".csv"), False)
+            write_challenges_to_file(challenges, os.path.join(solutiondir, name.strip() + "_solution.csv"), True)
         args.challenge_file.close()
 
 
-def write_challenges_to_file(challenges, file, header, show_collision):
+def write_challenges_to_file(challenges, file, show_collision):
     """Writes the given challenges to the given file as a Markdown table"""
     challenge_table = []
     counter = 1
-    for challenge in challenges:
-        collision = ''
-        if show_collision:
-            collision = challenge.password
-        challenge_table.append(collections.OrderedDict((('Number', counter),
-                                ('Points', challenge.points),
-                                ('Description', challenge.desc),
-                                ('Hash', challenge.digest),
-                                ('Alg', challenge.alg),
-                                ('Collision', collision),
-                                ('Method', ''))))
-        counter = counter + 1
-
-    file.write(header + "\n")
-    file.write(tabulate.tabulate(challenge_table, headers="keys", tablefmt="pipe"))
-    file.close()
+    with open(file, 'w', newline='') as f:
+        fieldnames = ['Number','Points','Description','Hash','Alg','Collision','Method' ]
+        writer = csv.writer(f)
+        writer.writerow(fieldnames)
+        for challenge in challenges:
+            collision = ''
+            if show_collision:
+                collision = challenge.password
+            writer.writerow([counter, challenge.points, challenge.desc, challenge.digest, challenge.alg, collision, ''])
+            counter = counter + 1
 
 def generate_challenges(challenge_file):
     challenges = []
